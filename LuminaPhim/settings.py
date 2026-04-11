d972ac4c08a5de2523ce77973c33179c512956ef
Moniwi <32587919+MoniLee@users.noreply.github.com>
2026-04-11 10:49:33 +0700

    Disable debug mode and allow all hosts

--- LuminaPhim/settings.py
+++ LuminaPhim/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l8=p*z1pu_1v=&3wx)&0p%*=nfhi++-1q(o(7e$pezj(y4h^a+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
