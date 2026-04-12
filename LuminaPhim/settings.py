e834ca4662605ac8d6bf284e1c473556158ed09a
MoniLee <lewinheo123@gmail.com>
2026-04-12 12:14:14 +0700

    Fix Cloudinary configuration

--- LuminaPhim/settings.py
+++ LuminaPhim/settings.py
@@ -171,19 +171,21 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Cloudinary Configuration for Media Files on Render
-if os.environ.get('CLOUDINARY_URL'):
-    import cloudinary
-    import cloudinary.uploader
-    import cloudinary.api
-    
+import cloudinary
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+if os.environ.get('CLOUDINARY_NAME') and os.environ.get('CLOUDINARY_API_KEY'):
     cloudinary.config(
         cloud_name=os.environ.get('CLOUDINARY_NAME'),
         api_key=os.environ.get('CLOUDINARY_API_KEY'),
         api_secret=os.environ.get('CLOUDINARY_API_SECRET')
     )
-    
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-    MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
